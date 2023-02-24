5fdda07244ac0d10ef9cdac54e4b9955133a0479
siyue2016 <126215802+siyue2016@users.noreply.github.com>
2023-02-24 13:33:17 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
 		System.out.println("master test");
 		System.out.println("hot fix test1");
 		System.out.println("push test1");
+                System.out.println("pull test1");
 	}
 }
